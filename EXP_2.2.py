4c93b2d968435796f30efb6b103dd60b6da9f558
Tushar Bhatia <tusharbhatia2103@gmail.com>
2024-02-02 10:52:22 +0530

    feat:Update

--- EXP_2.2.py
+++ EXP_2.2.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def is_valid_move(maze, x, y):
     rows, cols = len(maze), len(maze[0])
     return 0 <= x < rows and 0 <= y < cols and maze[x][y] == 0
@@ -22,24 +24,31 @@ def dfs(maze, start, end, path):
     return False
 
 def solve_maze(maze, start, end):
+    print("Random Maze")
+    print_maze(maze)
     path = []
     if dfs(maze, start, end, path):
         print("Path found:")
+        print_maze(maze)
         for step in reversed(path):
             print(step)
     else:
         print("No path found.")
 
+def print_maze(maze):
+    for row in maze:
+        print(" ".join(map(str, row)))
+
 # Example usage:
-maze = [
-    [0, 1, 0, 0, 0],
-    [0, 1, 0, 1, 0],
-    [0, 0, 0, 1, 0],
-    [0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0]
-]
+n = int(input("Enter the number dimension of maze: "))
+obstacle_count = float(input("Enter the obstacle density (between 0 and 1): "))
+maze = np.zeros((n, n), dtype=int)
+
+for i in range(n):
+    for j in range(n):
+        if np.random.rand() < obstacle_count:
+            maze[i, j] = 1
 
 start_point = (0, 0)
-end_point = (4, 4)
-
+end_point = (n - 1, n - 1)
 solve_maze(maze, start_point, end_point)
