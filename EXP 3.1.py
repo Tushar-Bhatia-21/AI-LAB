dc9834f86b9beec2dd27765d080eaa3553e48fd6
Tushar Bhatia <tusharbhatia2103@gmail.com>
2024-03-03 19:13:14 +0530

    feat: EXP 3 added

--- EXP 3.1.py
+++ EXP 3.1.py
@@ -1,55 +1,92 @@
-import numpy as np
-from collections import deque
+from queue import PriorityQueue
 
-def is_valid_move(maze, x, y):
-    rows, cols = len(maze), len(maze[0])
-    return 0 <= x < rows and 0 <= y < cols and maze[x][y] == 0
+class PuzzleNode:
+    def __init__(self, state, parent=None, move=None):
+        self.state = state
+        self.parent = parent
+        self.move = move
+        self.cost = 0
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
 
-def bfs(maze, start, end, path):
-    queue = deque([start])
-    visited = set([start])
+    def __lt__(self, other):
+        return self.cost < other.cost
 
-    while queue:
-        x, y = queue.popleft()
-        if (x, y) == end:
-            path.append((x, y))
-            return True
+    def __eq__(self, other):
+        return self.state == other.state
 
-        for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-            nx, ny = x + dx, y + dy
-            if is_valid_move(maze, nx, ny) and (nx, ny) not in visited:
-                queue.append((nx, ny))
-                visited.add((nx, ny))
-                maze[nx][ny] = 2  # Mark the cell as visited
+def is_goal(state):
+    return state == (1, 2, 3, 4, 5, 6, 7, 8, 0)
 
-    return False
+def get_manhattan_distance(state):
+    goal_state = (1, 2, 3, 4, 5, 6, 7, 8, 0)
+    distance = 0
+    for i in range(3):
+        for j in range(3):
+            value = state[i * 3 + j]
+            if value != 0:
+                goal_position = goal_state.index(value)
+                distance += abs(i - goal_position // 3) + abs(j - goal_position % 3)
+    return distance
 
-def solve_maze_bfs(maze, start, end):
-    print("Random Maze")
-    print_maze(maze)
-    path = []
-    if bfs(maze, start, end, path):
-        print("Path found:")
-        print_maze(maze)
-        for step in path:
-            print(step)
-    else:
-        print("No path found.")
+def get_possible_moves(state):
+    moves = []
+    zero_index = state.index(0)
+    row, col = zero_index // 3, zero_index % 3
+    if row > 0:
+        moves.append((-3, 'up'))
+    if row < 2:
+        moves.append((3, 'down'))
+    if col > 0:
+        moves.append((-1, 'left'))
+    if col < 2:
+        moves.append((1, 'right'))
+    return moves
 
-def print_maze(maze):
-    for row in maze:
-        print(" ".join(map(str, row)))
+def apply_move(state, move):
+    new_state = list(state)
+    zero_index = new_state.index(0)
+    new_index = zero_index + move[0]
+    new_state[zero_index], new_state[new_index] = new_state[new_index], new_state[zero_index]
+    return tuple(new_state)
+
+def solve_puzzle(initial_state):
+    start_node = PuzzleNode(initial_state)
+    start_node.cost = get_manhattan_distance(initial_state)
+    frontier = PriorityQueue()
+    frontier.put(start_node)
+
+    while not frontier.empty():
+        current_node = frontier.get()
+        current_state = current_node.state
+
+        if is_goal(current_state):
+            path = []
+            while current_node.parent:
+                path.append((current_node.move, current_node.state))
+                current_node = current_node.parent
+            path.reverse()
+            return path
+
+        possible_moves = get_possible_moves(current_state)
+        for move in possible_moves:
+            new_state = apply_move(current_state, move)
+            new_node = PuzzleNode(new_state, current_node, move[1])
+            new_node.cost = new_node.depth + get_manhattan_distance(new_state)
+            frontier.put(new_node)
+
+    return None
 
 # Example usage:
-n = int(input("Enter the number dimension of maze: "))
-obstacle_count = float(input("Enter the obstacle density (between 0 and 1): "))
-maze = np.zeros((n, n), dtype=int)
-
-for i in range(n):
-    for j in range(n):
-        if np.random.rand() < obstacle_count:
-            maze[i, j] = 1
-
-start_point = (0, 0)
-end_point = (n - 1, n - 1)
-solve_maze_bfs(maze, start_point, end_point)
+initial_state = (1, 2, 3, 4, 5, 6, 0, 7, 8)
+solution = solve_puzzle(initial_state)
+if solution:
+    for move, state in solution:
+        print(f"Move: {move}")
+        print(state[0:3])
+        print(state[3:6])
+        print(state[6:9])
+        print()
+else:
+    print("No solution found.")
--- EXP 3.2.py
+++ EXP 3.2.py
@@ -1,47 +1,82 @@
-import numpy as np
-from heapq import heappop, heappush
+from queue import PriorityQueue
 
 # Define the goal state
-goal_state = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+goal_state = (1, 2, 3, 4, 5, 6, 7, 8, 0)
 
-# Define heuristic function (Manhattan distance)
+# Define the heuristic function (Manhattan distance)
 def heuristic(state):
     distance = 0
     for i in range(3):
         for j in range(3):
-            if state[i][j] != 0:
-                goal_pos = np.where(goal_state == state[i][j])
-                distance += abs(i - goal_pos[0][0]) + abs(j - goal_pos[1][0])
+            if state[i*3 + j] != 0:
+                goal_row, goal_col = (state[i*3 + j] - 1) // 3, (state[i*3 + j] - 1) % 3
+                distance += abs(i - goal_row) + abs(j - goal_col)
     return distance
 
-# Define function to get possible moves
-def get_moves(state):
+# Define the puzzle state representation
+class PuzzleState:
+    def __init__(self, state, parent=None, move=None, depth=0):
+        self.state = state
+        self.parent = parent
+        self.move = move
+        self.depth = depth
+        self.cost = self.depth + heuristic(self.state)
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+# Define possible moves
+def possible_moves(state):
     moves = []
-    zero_pos = np.where(state == 0)
-    zero_x, zero_y = zero_pos[0][0], zero_pos[1][0]
-    for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-        new_x, new_y = zero_x + dx, zero_y + dy
-        if 0 <= new_x < 3 and 0 <= new_y < 3:
-            new_state = state.copy()
-            new_state[zero_x][zero_y], new_state[new_x][new_y] = new_state[new_x][new_y], new_state[zero_x][zero_y]
-            moves.append(new_state)
+    zero_index = state.index(0)
+    row, col = zero_index // 3, zero_index % 3
+    for dr, dc, move in [(1, 0, 'down'), (-1, 0, 'up'), (0, 1, 'right'), (0, -1, 'left')]:
+        new_row, new_col = row + dr, col + dc
+        if 0 <= new_row < 3 and 0 <= new_col < 3:
+            new_state = list(state)
+            new_state[row*3 + col], new_state[new_row*3 + new_col] = new_state[new_row*3 + new_col], new_state[row*3 + col]
+            moves.append((tuple(new_state), move))
     return moves
 
 # Define A* search algorithm
-def solve_puzzle(initial_state):
-    open_list = [(heuristic(initial_state), 0, initial_state)]
-    closed_list = set()
-    while open_list:
-        _, cost, state = heappop(open_list)
-        if np.array_equal(state, goal_state):
-            return cost
-        closed_list.add(tuple(map(tuple, state)))
-        for move in get_moves(state):
-            if tuple(map(tuple, move)) not in closed_list:
-                heappush(open_list, (cost + heuristic(move), cost + 1, move))
-    return -1
+def astar(start_state):
+    frontier = PriorityQueue()
+    explored = set()
+    start_node = PuzzleState(start_state)
+    frontier.put(start_node)
 
-# Example usage:
-initial_state = np.array([[1, 2, 3], [4, 5, 6], [0, 7, 8]])
-moves_needed = solve_puzzle(initial_state)
-print("Number of moves needed to solve the puzzle:", moves_needed)
+    while not frontier.empty():
+        current_node = frontier.get()
+        current_state = current_node.state
+        if current_state == goal_state:
+            path = []
+            while current_node.parent:
+                path.append((current_node.move, current_node.state))
+                current_node = current_node.parent
+            path.reverse()
+            return path
+        explored.add(current_state)
+
+        for next_state, move in possible_moves(current_state):
+            if next_state not in explored:
+                new_node = PuzzleState(next_state, current_node, move, current_node.depth + 1)
+                frontier.put(new_node)
+
+    return None
+
+# Define initial state
+initial_state = (1, 2, 3, 4, 5, 6, 0, 7, 8)
+
+# Solve the puzzle
+solution = astar(initial_state)
+
+# Print the solution
+if solution:
+    for move, state in solution:
+        print(f"Move: {move}")
+        print(state[0:3])
+        print(state[3:6])
+        print(state[6:9])
+        print()
+else:
+    print("No solution found.")
